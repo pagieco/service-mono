903e326a881abbf45f0b5c5aed1e8fce49b9250f
Jelle Spekken <jelle.spekken@centagon.com>
2019-08-05 20:34:50 +0200

    move api routes to api prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,6 +51,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::middleware('api')
+             ->prefix('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
