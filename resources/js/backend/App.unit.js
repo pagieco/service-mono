77c751135f96c645329514c1deccfae5b5a53f3f
Jelle Spekken <jelle.spekken@centagon.com>
2019-08-06 13:50:50 +0200

    move vue-meta titleTemplate getter to own exported function

--- resources/js/backend/App.unit.js
+++ resources/js/backend/App.unit.js
@@ -1,39 +1,20 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import VueRouter from 'vue-router';
 import appConfig from './app.config';
-import App from './App.vue';
-
-const localVue = createLocalVue();
-const router = new VueRouter();
-
-localVue.use(VueRouter);
+import App, { getPageTitle } from './App.vue';
 
 describe('<App />', () => {
   it('should be a valid vue component', () => {
     expect(App).toBeAComponent();
   });
 
-  it('should have the getPageTitle function', () => {
-    const wrapper = shallowMount(App, { localVue, router });
-
-    expect(wrapper.vm.getPageTitle).toBeInstanceOf(Function);
-  });
-
   it('should generate the initial page title when no arguments are passed into the getPageTitle function', () => {
-    const wrapper = shallowMount(App, { localVue, router });
-
-    expect(wrapper.vm.getPageTitle()).toEqual(appConfig.title);
+    expect(getPageTitle()).toEqual(appConfig.title);
   });
 
   it('should start with the given argument a string is passed into the getPageTitle function', () => {
-    const wrapper = shallowMount(App, { localVue, router });
-
-    expect(wrapper.vm.getPageTitle('Dashboard')).toMatch(/^Dashboard\s\|\s*/);
+    expect(getPageTitle('Dashboard')).toMatch(/^Dashboard\s\|\s*/);
   });
 
   it('should execute the function when a function is passed into the getPageTitle function', () => {
-    const wrapper = shallowMount(App, { localVue, router });
-
-    expect(wrapper.vm.getPageTitle(() => 'Page overview')).toMatch(/^Page overview\s\|\s*/);
+    expect(getPageTitle(() => 'Page overview')).toMatch(/^Page overview\s\|\s*/);
   });
 });
--- resources/js/backend/App.vue
+++ resources/js/backend/App.vue
@@ -2,23 +2,15 @@
 
 import appConfig from './app.config';
 
+export function getPageTitle(title, $store = null) {
+  const template = typeof title === 'function' ? title($store) : title;
+
+  return template ? `${template} | ${appConfig.title}` : appConfig.title;
+}
+
 export default {
   meta: {
-    titleTemplate: () => this.getPageTitle,
-  },
-
-  methods: {
-    /**
-     * Get the page's title.
-     *
-     * @param   {string | function} title
-     * @returns {string}
-     */
-    getPageTitle(title) {
-      const template = typeof title === 'function' ? title(this.$store) : title;
-
-      return template ? `${template} | ${appConfig.title}` : appConfig.title;
-    },
+    titleTemplate: title => getPageTitle(title, this.$store),
   },
 };
 
